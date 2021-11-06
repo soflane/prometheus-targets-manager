9abbca68cadc9c7938dc0193d827898febf3e8bc
Francis Makokha <francismakokha@gmail.com>
2021-11-06 18:00:27 +0100

    feat: Added custom field value logic to target add form

--- symfony/src/Controller/TargetController.php
+++ symfony/src/Controller/TargetController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\CustomField;
+use App\Entity\CustomFieldValue;
 use App\Entity\Target;
 use App\Form\Type\TargetType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,14 +16,33 @@ class TargetController extends AbstractController {
   #[Route('/target/add', name: 'target_add')]
   public function index(Request $request): Response {
     $target = new Target();
-    $form = $this->createForm(TargetType::class, $target);
+    $entityManager = $this->getDoctrine()->getManager();
+    $customFields = $entityManager->getRepository(CustomField::class)
+      ->findAll();
+    $form = $this->createForm(TargetType::class, $target, [
+      'customFields' => $customFields,
+    ]);
 
     $form->handleRequest($request);
     if ($form->isSubmitted() && $form->isValid()) {
       $target = $form->getData();
-
       $entityManager = $this->getDoctrine()->getManager();
+
       $entityManager->persist($target);
+
+      foreach ($customFields as $customField) {
+        $customFieldValue = new CustomFieldValue();
+        $customFieldValue->setCustomField($customField);
+        $customFieldValue->setTarget($target);
+        $value = $form->get(str_replace(' ', '-', $customField->getName()))
+          ->getData();
+
+        if (!empty($value)) {
+          $customFieldValue->setValue($value);
+          $entityManager->persist($customFieldValue);
+        }
+      }
+
       $entityManager->flush();
 
       return $this->redirectToRoute('target_add');
--- symfony/src/Form/Type/TargetType.php
+++ symfony/src/Form/Type/TargetType.php
@@ -4,7 +4,6 @@ namespace App\Form\Type;
 
 use App\Entity\Target;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
@@ -16,17 +15,23 @@ class TargetType extends AbstractType {
   public function buildForm(FormBuilderInterface $builder, array $options) {
     $builder
       ->add('url', UrlType::class)
-      ->add('targetGroup', TextType::class)
-      ->add('customFields', CollectionType::class, [
-        'entry_type' => TextType::class,
-      ])
-      ->add("submit", SubmitType::class);
-  }
+      ->add('targetGroup', TextType::class);
 
+    /** @var \App\Entity\CustomField $customField */
+    foreach ($options['customFields'] as $customField) {
+      $builder->add(str_replace(' ', '-', $customField->getName()), TextType::class, [
+        'mapped' => FALSE,
+      ]);
+    }
+
+    $builder->add("submit", SubmitType::class);
+  }
 
   public function configureOptions(OptionsResolver $resolver) {
     $resolver->setDefaults([
       'data_class' => Target::class,
+      'customFields' => NULL,
+      'allow_extra_fields' => TRUE,
     ]);
   }
 
